d2667eb79a0d003bf2c08105ec75afbe95c6b299
rustariaProgAlex <114904489+rustariaProgAlex@users.noreply.github.com>
2023-01-13 11:36:28 +0300

    new file:   shesterka/.gitignore 	new file:   shesterka/build.gradle 	new file:   shesterka/gradle/wrapper/gradle-wrapper.jar 	new file:   shesterka/gradle/wrapper/gradle-wrapper.properties 	new file:   shesterka/gradlew 	new file:   shesterka/gradlew.bat 	new file:   shesterka/settings.gradle 	new file:   shesterka/src/main/java/com/shesterka/shesterka/ShesterkaApplication.java 	new file:   shesterka/src/main/resources/application.properties 	new file:   shesterka/src/test/java/com/shesterka/shesterka/ShesterkaApplicationTests.java

---
+++ shesterka/src/test/java/com/shesterka/shesterka/ShesterkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shesterka.shesterka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShesterkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
