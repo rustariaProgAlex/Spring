3d455bec0cbfea8feedb29206397cf5630b82a26
rustariaProgAlex <114904489+rustariaProgAlex@users.noreply.github.com>
2023-01-13 11:21:05 +0300

    first commit

---
+++ shesterka/src/main/java/com/shesterka/shesterka/ShesterkaApplication.java
@@ -0,0 +1,13 @@
+package com.shesterka.shesterka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShesterkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShesterkaApplication.class, args);
+	}
+
+}
